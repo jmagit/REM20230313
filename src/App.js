c3867d4b2de7d9f8c165eb8ebb880ea71d0d6965
Javier <jmartin@grupoloyal.com>
2023-03-13 14:05:29 +0100

    Ejemplos

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nombre = 'Canarias'
+  const elemento = <h1>Hola {nombre}</h1>
+  const fn = nom => <h1>Hola {nom}</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hola {nombre.toUpperCase()}
+          </a>
+        </header>
+      </div>
+      {elemento}
+      {fn('tu')}
+    </>);
 }
 
 export default App;
