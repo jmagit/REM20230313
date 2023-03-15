c212f27b77aec2a220400a04a0d0bb0329bdff19
Javier <jmartin@grupoloyal.com>
2023-03-15 11:14:46 +0100

    Menu variable

--- src/App.js
+++ src/App.js
@@ -52,50 +52,13 @@ function Head() {
 function Menu(props) {
   return (
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-      <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">
-          Home
+      {props.opciones.map((item, index) => (
+      <li key={index} className="nav-item">
+        <a className="nav-link" href={item.url}>
+          {item.texto}
         </a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item dropdown">
-        <a
-          className="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Dropdown
-        </a>
-        <ul className="dropdown-menu">
-          <li>
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled">Disabled</a>
-      </li>
+      ))}
     </ul>
   );
 }
