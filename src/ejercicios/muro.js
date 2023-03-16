24f503259342889f264bc3699bfa618a37efb15e
Javier <jmartin@grupoloyal.com>
2023-03-16 14:02:12 +0100

    fetch

--- src/ejercicios/muro.js
+++ src/ejercicios/muro.js
@@ -6,15 +6,44 @@ export default class Muro extends Component {
         super(props);
         this.state = { 
             listado: [], 
+            errorMsg: null,
             loading: true 
         };
+    }
+
+    load(pagina = 0) {
+        this.setState({ loading: true, errorMsg: null  })
+        fetch(`https://picsum.photos/v2/lista?page=${pagina}&limit=20`)
+            .then(
+                resp => {
+                    if(resp.ok) {
+                        resp.json().then(
+                            data => this.setState({ listado: data, loading: false }),
+                            err2 => this.setState({ errorMsg: err2.status, loading: false })
+                        )
+                    } else {
+                        this.setState({ errorMsg: resp.status, loading: false })
+                    }
+                },
+                err => {
+                    this.setState({ errorMsg: err.status, loading: false })
+                }
+            )
+
     }
 
     render() {
         if(this.state.loading)
             return <Esperando />
         return (
-            <div>Muro</div>
+            <div>
+                {this.state.errorMsg}
+                {JSON.stringify(this.state.listado)}
+            </div>
         )
+    }
+
+    componentDidMount() {
+        this.load();
     }
 }
